b309278ae5cfb56fd6292049df66cb55f8033140
Frazier <mrfrazier628@hotmail.com>
2025-10-12 15:13:09 -0400

    added get price function

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,25 +5,37 @@
     title: string;
     description: string;
     price: number;
-    discountPercentage: number | undefined;
+    discountPercentage: number;
+    discountAmount: number;
+    
 
-    constructor(id: number, title: string, description: string, price: number, discountPercentage: number | undefined) {
+    constructor(id: number, title: string, description: string, price: number, discountPercentage: number, discountAmount: number) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.price = price;
-        this.discountPercentage; discountPercentage
+        this.discountPercentage = discountPercentage;
+        this.discountAmount = discountAmount;
+        
     }
 
-    displayDetails()  {
-        return `${this.title} 'is about software, and cost' $${this.price} "before the" ${this.discountPercentage}.`
+    displayDetails(): string  {
+        return `The book id is ${this.id} and the title is: ${this.title} is about ${this.description}, and cost' $${this.price} before the ${this.discountPercentage}% discount total.`;
     }
 
-    getPriceWithDiscount() {
-        return
+    getPriceWithDiscount(): number | string {
+        if (this.price >= 100) {
+            const disc = this.price * (this.discountPercentage);
+            return `The discount amount is: $${disc}`;
+        } else {
+            return this.price;
+        }
     }
+
 
  }
-
+    const discRate = new Product(7787, "TypeScript", "OOP", 150, .10, .08)
+    const book = new Product(12344, "Software", "About Coding", 40, 5, .15);
+    console.log(book.displayDetails());
+    console.log(discRate.getPriceWithDiscount())
     
-    console.log(Product.name);
